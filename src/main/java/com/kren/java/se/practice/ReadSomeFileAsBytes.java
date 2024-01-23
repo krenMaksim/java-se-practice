1b89949f62a0b0f627c55e01d58256f11cb33d27
krenMaksim <krenmaba@gmail.com>
2024-01-23 06:45:48 +0300

    refactored ReadSomeFileAsBytes

--- src/main/java/com/kren/java/se/practice/ReadSomeFileAsBytes.java
+++ src/main/java/com/kren/java/se/practice/ReadSomeFileAsBytes.java
@@ -5,17 +5,31 @@ import java.io.IOException;
 
 class ReadSomeFileAsBytes {
 
-	public static void main(String[] args) throws IOException {
-		String filePath = ReadSomeFileAsBytes.class.getClassLoader().getResource("some_file.txt").getFile();
-		try (FileInputStream input = new FileInputStream(filePath)) {
-			System.out.println("available bytes: " + 	input.available());
+  public static void main(String[] args) throws IOException {
+    var filePath = getFilePath("some_file.txt");
+    readFileAsBytes(filePath);
+  }
 
-			int i = input.read();
-			while (i != -1) {
-				System.out.print((char) i);
-				i = input.read();
-			}
-		}
-	}
+  private static void readFileAsBytes(String filePath) throws IOException {
+    try (var input = new FileInputStream(filePath)) {
+      System.out.println("Available bytes: " + input.available());
+      var i = input.read();
+      while (i != -1) {
+        System.out.print((char) i);
+        i = input.read();
+      }
+    }
+  }
 
+  private static String getFilePath(String fileName) {
+    return ReadSomeFileAsBytes.class.getClassLoader()
+        .getResource(fileName)
+        .getFile();
+  }
+
+  private static String toBinary(int readByte) {
+    // https://stackoverflow.com/questions/12310017/how-to-convert-a-byte-to-its-binary-string-representation
+    var b1 = (byte) readByte;
+    return String.format("%8s", Integer.toBinaryString(b1 & 0xFF)).replace(' ', '0');
+  }
 }
