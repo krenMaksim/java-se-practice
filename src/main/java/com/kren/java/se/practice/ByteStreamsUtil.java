980512b9859cc368b65809899fa4caeb2ea07338
krenMaksim <krenmaba@gmail.com>
2024-01-24 08:58:12 +0300

    refactored ByteStreamsUtil

--- src/main/java/com/kren/java/se/practice/ByteStreamsUtil.java
+++ src/main/java/com/kren/java/se/practice/ByteStreamsUtil.java
@@ -2,7 +2,9 @@ package com.kren.java.se.practice;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class ByteStreamsUtil {
 
@@ -16,5 +18,21 @@ public class ByteStreamsUtil {
         System.out.print((char) byteOfData);
       }
     }
+  }
+
+  public static void writeBytesToFile(InputStream input, File file) throws IOException {
+    var output = new FileOutputStream(file);
+    try (input; output) {
+      int i;
+      while ((i = input.read()) != EOF_BYTE) {
+        output.write(i);
+      }
+    }
+  }
+
+  private static String toBinary(int oneByte) {
+    // https://stackoverflow.com/questions/12310017/how-to-convert-a-byte-to-its-binary-string-representation
+    var b1 = (byte) oneByte;
+    return String.format("%8s", Integer.toBinaryString(b1 & 0xFF)).replace(' ', '0');
   }
 }
--- src/test/java/com/kren/java/se/practice/ByteStreamsUtilTest.java
+++ src/test/java/com/kren/java/se/practice/ByteStreamsUtilTest.java
@@ -2,6 +2,7 @@ package com.kren.java.se.practice;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.Paths;
 
@@ -14,5 +15,13 @@ class ByteStreamsUtilTest {
     var file = Paths.get("src", "test", "resources", "some_file.txt").toFile();
 
     assertDoesNotThrow(() -> ByteStreamsUtil.readFileAsBytes(file));
+  }
+
+  @Test
+  void writeBytesToFile() {
+    var bytes = new ByteArrayInputStream(new byte[] {100, 103, 15});
+    var file = Paths.get("target", "write_file.txt").toFile();
+
+    assertDoesNotThrow(() -> ByteStreamsUtil.writeBytesToFile(bytes, file));
   }
 }
