0b6d358b024296ffc933951eb59b326e10a7ad72
krenMaksim <krenmaba@gmail.com>
2024-01-29 08:51:28 +0300

    done ReadingOperation

--- src/test/java/com/kren/java/se/practice/BufferedVsNotBufferedStreamsTest.java
+++ src/test/java/com/kren/java/se/practice/BufferedVsNotBufferedStreamsTest.java
@@ -3,9 +3,9 @@ package com.kren.java.se.practice;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -18,50 +18,56 @@ import java.util.List;
 import java.util.function.IntConsumer;
 
 import static com.kren.java.se.practice.ByteStreamsUtil.readInputStream;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BufferedVsNotBufferedStreamsTest {
 
   @Nested
-  @TestInstance(PER_CLASS)
   class ReadingOperation {
 
-    private File file;
+    private static final int FILE_SIZE_MB = 10;
+    private static final int FILE_SIZE_BYTE = FILE_SIZE_MB * 1_000_000;
+
+    private static File file;
 
     @BeforeAll
-    void setUp() {
+    static void createFile() {
       file = Paths.get("src", "test", "resources", "buffered_vs_not_buffered_streams.txt").toFile();
-      DataGeneratorUtil.writeRandomBytes(file, 10);
+      DataGeneratorUtil.writeRandomBytes(file, FILE_SIZE_MB);
+    }
+
+    private List<Integer> data;
+    IntConsumer byteProcessor;
+
+    @BeforeEach
+    void setUp() {
+      data = new LinkedList<>();
+      byteProcessor = byteOfData -> data.add(byteOfData);
     }
 
     @Test
     @SneakyThrows
     void readFile() {
-      List<Integer> data = new LinkedList<>();
-      IntConsumer byteProcessor = byteOfData -> data.add(byteOfData);
-
       var input = new FileInputStream(file);
 
-      assertDoesNotThrow(() -> readInputStream(input, byteProcessor));
-      System.out.println("Read bytes:" + data.size());
+      readInputStream(input, byteProcessor);
+
+      assertEquals(FILE_SIZE_BYTE, data.size());
     }
 
     @SneakyThrows
     @ParameterizedTest
     @ValueSource(ints = {1024, 4096, 8192, 16384, 32768, 65536})
     void readFileBuffered(int dufferSizeBytes) {
-      List<Integer> data = new LinkedList<>();
-      IntConsumer byteProcessor = byteOfData -> data.add(byteOfData);
-
       var input = new BufferedInputStream(new FileInputStream(file), dufferSizeBytes);
 
-      assertDoesNotThrow(() -> readInputStream(input, byteProcessor));
-      System.out.println("Read bytes:" + data.size());
+      readInputStream(input, byteProcessor);
+
+      assertEquals(FILE_SIZE_BYTE, data.size());
     }
 
     @AfterAll
-    void tearDown() {
+    static void deleteFile() {
       file.delete();
     }
   }
