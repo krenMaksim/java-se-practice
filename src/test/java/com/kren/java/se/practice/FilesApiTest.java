4ff7032e059153ff079ccd27ad3e553db938dfac
krenMaksim <krenmaba@gmail.com>
2024-01-31 09:45:20 +0300

    done with refactoring FilesApiTest

--- src/test/java/com/kren/java/se/practice/FilesApiTest.java
+++ src/test/java/com/kren/java/se/practice/FilesApiTest.java
@@ -1,7 +1,8 @@
 package com.kren.java.se.practice;
 
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Files;
@@ -18,99 +19,99 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FilesApiTest {
 
-  private static final int FILE_SIZE_MB = 1;
-  private static final int FILE_SIZE_BYTE = FILE_SIZE_MB * 1_000_000;
+  private Path content;
+  private Path targetFile;
 
-  private static Path file;
+  private long contentSizeBytes;
 
-  @BeforeAll
-  static void createFile() {
-    file = Paths.get("target", "file_api.txt");
-    DataGeneratorUtil.writeRandomBytes(file.toFile(), FILE_SIZE_MB);
+  @BeforeEach
+  @SneakyThrows
+  void setUp() {
+    content = Paths.get("src", "test", "resources", "some_file.txt");
+    targetFile = Paths.get("target", "target_file_api.txt");
+    contentSizeBytes = Files.size(content);
+  }
+
+  @AfterEach
+  @SneakyThrows
+  void tearDown() {
+    Files.deleteIfExists(targetFile);
   }
 
   @Test
   @SneakyThrows
-  void readInMemory() {
-    var bytes = Files.readAllBytes(file);
+  void readContentAsBytesInMemory() {
+    var bytes = Files.readAllBytes(content);
 
-    assertEquals(FILE_SIZE_BYTE, bytes.length);
+    assertEquals(contentSizeBytes, bytes.length);
   }
 
   @Test
   @SneakyThrows
-  void writeInMemory() {
-    var bytes = Files.readAllBytes(file);
-    var targetFile = Paths.get("target", "target_file_api.txt");
+  void readContentAsLinesInMemory() {
+    assertDoesNotThrow(() -> {
+      Files.readAllLines(content)
+          .stream()
+          .forEach(System.out::println);
+    });
+  }
+
+  @Test
+  @SneakyThrows
+  void readContentAsSingleString() {
+    var str = Files.readString(content);
+
+    assertEquals(contentSizeBytes, str.length());
+  }
+
+  @Test
+  @SneakyThrows
+  void writeContentAsBytesToFile() {
+    var bytes = Files.readAllBytes(content);
 
     Files.write(targetFile, bytes);
 
-    assertEquals(bytes.length, Files.size(targetFile));
-    Files.delete(targetFile);
+    assertEquals(contentSizeBytes, Files.size(targetFile));
   }
 
   @Test
   @SneakyThrows
-  void createInputStream() {
-    var someFile = Paths.get("src", "test", "resources", "some_file.txt");
-    var input = Files.newInputStream(someFile);
+  void readContentAsInputStream() {
+    var input = Files.newInputStream(content);
 
     assertDoesNotThrow(() -> ByteStreamsUtil.readInputStream(input));
   }
 
   @Test
   @SneakyThrows
-  void readFileLineByLineViaStream() {
-    var someFile = Paths.get("src", "test", "resources", "some_file.txt");
-
-    Files.lines(someFile)
-        .forEach(System.out::println);
+  void readContentLineByLineViaStreamApi() {
+    assertDoesNotThrow(() -> {
+      Files.lines(content)
+          .forEach(System.out::println);
+    });
   }
 
   @Test
   @SneakyThrows
-  void readFileLineByLineViaBufferedReader() {
-    var someFile = Paths.get("src", "test", "resources", "some_file.txt");
-    
-    try (var reader = Files.newBufferedReader(someFile)) {
-      String line;
-      while ((line = reader.readLine()) != null) {
-        System.out.println(line);
+  void readContentLineByLineViaBufferedReader() {
+    assertDoesNotThrow(() -> {
+      try (var reader = Files.newBufferedReader(content)) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+          System.out.println(line);
+        }
       }
-    }
+    });
   }
 
   @Test
   @SneakyThrows
-  void inMemoryReadAllLines() {
-    var someFile = Paths.get("src", "test", "resources", "some_file.txt");
+  void readAndWriteContentViaBufferedStreams() {
+    var reader = Files.newBufferedReader(content);
+    var writer = Files.newBufferedWriter(targetFile);
 
-    Files.readAllLines(someFile)
-        .stream()
-        .forEach(System.out::println);
-  }
+    CharacterStreamsUtil.writeCharacters(reader, writer);
 
-  @Test
-  @SneakyThrows
-  void readAllAsString() {
-    var someFile = Paths.get("src", "test", "resources", "some_file.txt");
-
-    var result = Files.readString(someFile);
-
-    System.out.println(result);
-  }
-
-  @Test
-  @SneakyThrows
-  void readAndWriteCharactersViaBufferedStreams() {
-    var someFile = Paths.get("src", "test", "resources", "some_file.txt");
-    var targetFile = Paths.get("target", "target_file_api.txt");
-    var input = Files.newBufferedReader(someFile);
-    var output = Files.newBufferedWriter(targetFile);
-
-    CharacterStreamsUtil.writeCharacters(input, output);
-
-    assertEquals(Files.size(someFile), Files.size(targetFile));
-    Files.delete(targetFile);
+    assertEquals(contentSizeBytes, Files.size(targetFile));
   }
 }
