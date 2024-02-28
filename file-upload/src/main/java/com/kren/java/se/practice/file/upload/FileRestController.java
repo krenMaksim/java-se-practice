52e1cb465d57e5fd314d9e6c4518d7578c7a75eb
krenMaksim <krenmaba@gmail.com>
2024-02-28 09:13:27 +0300

    get something working for file uploading

--- file-upload/src/main/java/com/kren/java/se/practice/file/upload/FileRestController.java
+++ file-upload/src/main/java/com/kren/java/se/practice/file/upload/FileRestController.java
@@ -1,16 +1,33 @@
 package com.kren.java.se.practice.file.upload;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @RestController
 class FileRestController {
 
   @PostMapping("/upload-file-form")
   public String uploadFile(@RequestParam("file") MultipartFile file) {
+    log.info("Uploaded {}", file.getOriginalFilename());
+    readInputStream(file);
     return file.getOriginalFilename();
+  }
+
+  private static final int EOF_BYTE = -1;
+
+  @SneakyThrows
+  private static void readInputStream(MultipartFile file) {
+    try (var input = file.getInputStream()) {
+      int byteOfData;
+      while ((byteOfData = input.read()) != EOF_BYTE) {
+        System.out.print((char) byteOfData);
+      }
+    }
   }
 
   // TBD
