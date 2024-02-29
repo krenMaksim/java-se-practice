193fddadde81867c79ce91f03918d3ef11dd8423
krenMaksim <krenmaba@gmail.com>
2024-02-29 10:39:03 +0300

    allocated FileUploadTestClient:

--- file-upload/src/test/java/com/kren/java/se/practice/file/upload/FileRestControllerTest.java
+++ file-upload/src/test/java/com/kren/java/se/practice/file/upload/FileRestControllerTest.java
@@ -1,8 +1,10 @@
 package com.kren.java.se.practice.file.upload;
 
-import com.kren.java.se.practice.io.DataGeneratorUtil;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +14,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
@@ -27,28 +30,26 @@ class FileRestControllerTest {
   private static File file;
 
   @BeforeAll
+  @SneakyThrows
   static void createTestFile() {
     file = Paths.get("target", "random_bytes_data.txt").toFile();
-    DataGeneratorUtil.writeRandomBytes(file, 1);
+    file.createNewFile(); // temp solution to run tests faster during development
+    // DataGeneratorUtil.writeRandomBytes(file, 1);
   }
 
   @Autowired
   private TestRestTemplate restTemplate;
 
+  private FileUploadTestClient client;
+
+  @BeforeEach
+  void setUp() {
+    client = new FileUploadTestClient(restTemplate);
+  }
+
   @Test
   void uploadFile() {
-    var headers = new HttpHeaders();
-    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
-    var body = new LinkedMultiValueMap<String, Object>();
-    body.add("file", new FileSystemResource(file));
-
-    var requestEntity = new HttpEntity<MultiValueMap<String, Object>>(body, headers);
-
-    var response = restTemplate.postForEntity("/upload-file-form", requestEntity, String.class);
-
-    response.getStatusCode();
-    response.getBody();
+    var response = client.uploadFile(file);
 
     assertThat(response.getStatusCode(), is(HttpStatus.OK));
     assertThat(response.getBody(), is("random_bytes_data.txt"));
@@ -57,5 +58,23 @@ class FileRestControllerTest {
   @AfterAll
   static void deleteTestFile() {
     file.delete();
+  }
+
+  @AllArgsConstructor
+  static class FileUploadTestClient {
+
+    private final TestRestTemplate restTemplate;
+
+    public ResponseEntity<String> uploadFile(File file) {
+      var headers = new HttpHeaders();
+      headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+      var body = new LinkedMultiValueMap<String, Object>();
+      body.add("file", new FileSystemResource(file));
+
+      var requestEntity = new HttpEntity<MultiValueMap<String, Object>>(body, headers);
+
+      return restTemplate.postForEntity("/upload-file-form", requestEntity, String.class);
+    }
   }
 }
