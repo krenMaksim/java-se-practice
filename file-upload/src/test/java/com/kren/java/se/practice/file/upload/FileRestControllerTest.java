b11fb646fe55a4aa028083a52786dc6469325985
krenMaksim <krenmaba@gmail.com>
2024-02-29 10:44:37 +0300

    done some refactoring

--- file-upload/src/test/java/com/kren/java/se/practice/file/upload/FileRestControllerTest.java
+++ file-upload/src/test/java/com/kren/java/se/practice/file/upload/FileRestControllerTest.java
@@ -1,6 +1,6 @@
 package com.kren.java.se.practice.file.upload;
 
-import lombok.AllArgsConstructor;
+import com.kren.java.se.practice.file.upload.test.util.FileUploadTestClient;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,41 +9,34 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import java.io.File;
 import java.nio.file.Paths;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class FileRestControllerTest {
+
+  private static final String FILE_NAME = "random_bytes_data.txt";
 
   private static File file;
 
   @BeforeAll
   @SneakyThrows
   static void createTestFile() {
-    file = Paths.get("target", "random_bytes_data.txt").toFile();
+    file = Paths.get("target", FILE_NAME).toFile();
     file.createNewFile(); // temp solution to run tests faster during development
     // DataGeneratorUtil.writeRandomBytes(file, 1);
   }
 
-  @Autowired
-  private TestRestTemplate restTemplate;
-
   private FileUploadTestClient client;
 
   @BeforeEach
-  void setUp() {
+  void setUp(@Autowired TestRestTemplate restTemplate) {
     client = new FileUploadTestClient(restTemplate);
   }
 
@@ -52,29 +45,11 @@ class FileRestControllerTest {
     var response = client.uploadFile(file);
 
     assertThat(response.getStatusCode(), is(HttpStatus.OK));
-    assertThat(response.getBody(), is("random_bytes_data.txt"));
+    assertThat(response.getBody(), is(FILE_NAME));
   }
 
   @AfterAll
   static void deleteTestFile() {
     file.delete();
-  }
-
-  @AllArgsConstructor
-  static class FileUploadTestClient {
-
-    private final TestRestTemplate restTemplate;
-
-    public ResponseEntity<String> uploadFile(File file) {
-      var headers = new HttpHeaders();
-      headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
-      var body = new LinkedMultiValueMap<String, Object>();
-      body.add("file", new FileSystemResource(file));
-
-      var requestEntity = new HttpEntity<MultiValueMap<String, Object>>(body, headers);
-
-      return restTemplate.postForEntity("/upload-file-form", requestEntity, String.class);
-    }
   }
 }
---
+++ file-upload/src/test/java/com/kren/java/se/practice/file/upload/test/util/FileUploadTestClient.java
@@ -0,0 +1,31 @@
+package com.kren.java.se.practice.file.upload.test.util;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.io.File;
+
+@AllArgsConstructor
+public class FileUploadTestClient {
+
+  private final TestRestTemplate restTemplate;
+
+  public ResponseEntity<String> uploadFile(File file) {
+    var headers = new HttpHeaders();
+    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+    var body = new LinkedMultiValueMap<String, Object>();
+    body.add("file", new FileSystemResource(file));
+
+    var requestEntity = new HttpEntity<MultiValueMap<String, Object>>(body, headers);
+
+    return restTemplate.postForEntity("/upload-file-form", requestEntity, String.class);
+  }
+}
