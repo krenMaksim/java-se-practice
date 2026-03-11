b99ab3036845d7f7a1588539177b31fd73ef7ae6
krenMaksim <krenmaba@gmail.com>
2026-03-11 07:02:46 +0300

    Push recent changes

--- collections/src/test/java/com/kren/java/se/practice/collections/QueueTest.java
+++ collections/src/test/java/com/kren/java/se/practice/collections/QueueTest.java
@@ -3,6 +3,7 @@ package com.kren.java.se.practice.collections;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.PriorityQueue;
 
 class QueueTest {
@@ -11,7 +12,7 @@ class QueueTest {
   void checkArrayDeque() {
     var deque = new ArrayDeque<String>();
 
-    deque.size(); // it's strange that it has limit.
+    deque.size();
   }
 
   @Test
@@ -19,5 +20,18 @@ class QueueTest {
     var priorityQueue = new PriorityQueue<String>();
 
     priorityQueue.size();
+  }
+
+  @Test
+  void checkStack() {
+    Deque<String> stack = new ArrayDeque<>();
+
+    stack.push("ttt");
+    stack.push("nnn");
+    stack.push("ddd");
+
+    while (!stack.isEmpty()) {
+      System.out.println(stack.pollFirst());
+    }
   }
 }
